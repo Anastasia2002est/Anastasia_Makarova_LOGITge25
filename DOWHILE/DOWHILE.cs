3666650e990b02d589f74c3a3dd553f2b15b2a2d
Anastasia2002est <anastasia.Makarova@tthkee.onmicrosoft.com>
2026-03-12 13:06:21 +0200

    H

---
+++ DOWHILE/DOWHILE.cs
@@ -0,0 +1,32 @@
+﻿namespace DOWHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Ülesanne 3 */
+            Console.WriteLine("Mis on sinu viimased mängimis tundid?");
+            float tunde = float.Parse(Console.ReadLine());
+            if (tunde < 0.00)
+            {
+                Console.WriteLine("kahju et sul aega mängida pole :c");
+            }
+            else if (tunde <= 10 && tunde >= 0)
+            {
+                Console.WriteLine("mõõdukas mängija, tubli");
+            }
+            else if (tunde <= 20 && tunde >= 10)
+            {
+                Console.WriteLine("natuke paljuks läheb, kodutööd kannatavad");
+            }
+            else if (tunde <= 40 && tunde >= 20)
+            {
+                Console.WriteLine("liiga palju mängid, kas sa kooli üldse nii jõuad?");
+            }
+            else if (tunde <= 999 && tunde >= 40)
+            {
+                Console.WriteLine("siin peaks juba sekkuma psühholoog");
+            }
+        }
+    }
+}
