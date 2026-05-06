e5fdd53bd7b0fd788c17231cef0d2135e5a588b9
Anastasia2002est <anastasia.Makarova@tthkee.onmicrosoft.com>
2026-05-06 20:45:15 +0300

    hihi

---
+++ Meetodiharjutusele 7/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿dhgkehgjheaghakjghkaehgr.anamespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
