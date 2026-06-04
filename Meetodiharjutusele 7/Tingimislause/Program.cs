6e304f741a15c5dbdc3e538418845c3245afe5d7
Anastasia2002est <anastasia.Makarova@tthkee.onmicrosoft.com>
2026-06-04 12:02:51 +0300

    Update Program.cs

--- Meetodiharjutusele 7/Tingimislause/Program.cs
+++ Meetodiharjutusele 7/Tingimislause/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace Tingimislause
+namespace Tingimuslausetöö
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* ülesanne 1 */
+            /* Console.WriteLine("Kas sa elad Kadaka Tänaval?");
+            string vastus = Console.ReadLine();
+
+            // Kontrollime, kas kasutaja vastas "jah"
+            // .ToLower() aitab juhul, kui kasutaja kirjutab "Jah" suure tähega
+            if (vastus.ToLower() == "jah")
+            {
+                Console.WriteLine("Kas kadakast vestad ka midagi?");
+            } */
+
+            /* ülesanne 2 */
+            /* Console.WriteLine("Kas sulle meeldib vaniljejäätis?");
+            string vastus = Console.ReadLine().ToLower();
+            if (vastus == "jah")
+            {
+                Console.WriteLine("Vanilla ninja jäätis on most basic");
+            }
+            else
+            {
+                Console.WriteLine("Vähemalt pole basic maitse sul"); 
+            } */
+
+            /* Ülesanne 3 */
+            /* Console.WriteLine("Mis on sinu viimased õppetundide tunnid?");
+            float tunde = float.Parse(Console.ReadLine());
+
+            if (tunde == 0) // Kontrollib täpselt nulli
+            {
+                Console.WriteLine("Kahju et sa endast lugu ei pea :c");
+            }
+            else if (tunde > 0 && tunde <= 5) // Vahemik 0.1 kuni 5
+            {
+                Console.WriteLine("Algus on hea, aga kõike nii tehtud ei saa");
+            }
+            else if (tunde > 5 && tunde <= 10) // Vahemik 5.1 kuni 10
+            {
+                Console.WriteLine("OH, sellise tööhooga saab strippigi");
+            }
+            else if (tunde > 10 && tunde <= 20)
+            {
+                Console.WriteLine("Kuldmedal aasta lõpus?");
+            }
+            else if (tunde > 20)
+            {
+                Console.WriteLine("Siin peaks juba sekkuma psühholoog");
+            } */
+
+            /* Ülesanne 4 */
+
+
+
+
+
+
         }
     }
 }
